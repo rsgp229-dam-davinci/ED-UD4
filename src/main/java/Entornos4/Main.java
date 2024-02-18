d6f7f81dd8738acd598f8d45273ddc1390652b0e
rsgp229-dam-davinci <rsgp229.elastic262@passmail.net>
2024-02-18 20:24:36 +0100

    Corrigiendo varios errores y añadiendo la totalidad del javadoc generado

--- src/main/java/Entornos4/Main.java
+++ src/main/java/Entornos4/Main.java
@@ -2,14 +2,25 @@ package Entornos4;
 
 import java.util.random.RandomGenerator;
 
+/***
+ * Clase principal del programa
+ * @author Rafael SGP
+ */
 public class Main {
 
+    /***
+     * Método main y punto de entrada del programa
+     * @param args Parámetros obtenidos desde la línea de comandos
+     */
     public static void main(String[] args) {
 
         NuevoMetodo();
 
     }
 
+    /***
+     * Método refactorizado extrayendo el código que estaba en main
+     */
     public static void NuevoMetodo() {
         eCesta cesta;
 
@@ -54,6 +65,10 @@ public class Main {
         System.out.println("Su bono actual es  " + bonoactual);
     }
     
+    /***
+     * Método que devuelve un pseudoaletorio utilizando un algoritmo más actual
+     * @return Número pseudoaleatorio generado
+     */
     public static long getRandom(){
         RandomGenerator g = RandomGenerator.of("L128X1024MixRandom");
         return g.nextLong();
